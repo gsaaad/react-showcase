80489fe2d226f8592ffa42fb0cf32e178e959151
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-28 10:17:37 -0400

    added background color flipper with simple colors (built in css) as well as random HEX color generator

--- src/components/BG_simple-Flipper/BG_Flipper.js
+++ src/components/BG_simple-Flipper/BG_Flipper.js
@@ -2,27 +2,161 @@ import React from "react";
 import "./BG_Flipper.css";
 
 const BgFlipper = () => {
-  const handlebgflip = (e) => {
+  const getColor = () => {
+    return "#" + Math.random().toString(16).slice(2, 8);
+  };
+  const handleBgFlip = (e) => {
     e.preventDefault();
     const simpleColorArr = [
-      "red",
-      "green",
-      "blue",
-      "yellow",
-      "pink",
-      "white",
-      "black",
-      "purple",
-      "brown",
-      "firebrick",
-      "orange",
-      "orangered",
-      "yellowgreen",
-      "palegreen",
-      "forestgreen",
-      "crimson",
-      "cyan",
-      "salmon",
+      "AliceBlue",
+      "AntiqueWhite",
+      "Aqua",
+      "Aquamarine",
+      "Azure",
+      "Beige",
+      "Bisque",
+      "Black",
+      "BlanchedAlmond",
+      "Blue",
+      "BlueViolet",
+      "Brown",
+      "BurlyWood",
+      "CadetBlue",
+      "Chartreuse",
+      "Chocolate",
+      "Coral",
+      "CornflowerBlue",
+      "Cornsilk",
+      "Crimson",
+      "Cyan",
+      "DarkBlue",
+      "DarkCyan",
+      "DarkGoldenRod",
+      "DarkGray",
+      "DarkGrey",
+      "DarkGreen",
+      "DarkKhaki",
+      "DarkMagenta",
+      "DarkOliveGreen",
+      "DarkOrange",
+      "DarkOrchid",
+      "DarkRed",
+      "DarkSalmon",
+      "DarkSeaGreen",
+      "DarkSlateBlue",
+      "DarkSlateGray",
+      "DarkSlateGrey",
+      "DarkTurquoise",
+      "DarkViolet",
+      "DeepPink",
+      "DeepSkyBlue",
+      "DimGray",
+      "DimGrey",
+      "DodgerBlue",
+      "FireBrick",
+      "FloralWhite",
+      "ForestGreen",
+      "Fuchsia",
+      "Gainsboro",
+      "GhostWhite",
+      "Gold",
+      "GoldenRod",
+      "Gray",
+      "Grey",
+      "Green",
+      "GreenYellow",
+      "HoneyDew",
+      "HotPink",
+      "IndianRed",
+      "Indigo",
+      "Ivory",
+      "Khaki",
+      "Lavender",
+      "LavenderBlush",
+      "LawnGreen",
+      "LemonChiffon",
+      "LightBlue",
+      "LightCoral",
+      "LightCyan",
+      "LightGolden",
+      "RodYellow",
+      "LightGray",
+      "LightGrey",
+      "LightGreen",
+      "LightPink",
+      "LightSalmon",
+      "LightSeaGreen",
+      "LightSkyBlue",
+      "LightSlateGray",
+      "LightSlateGrey",
+      "LightSteelBlue",
+      "LightYellow",
+      "Lime",
+      "LimeGreen",
+      "Linen",
+      "Magenta",
+      "Maroon",
+      "MediumAquaMarine",
+      "MediumBlue",
+      "MediumOrchid",
+      "MediumPurple",
+      "MediumSeaGreen",
+      "MediumSlateBlue",
+      "MediumSpringGreen",
+      "MediumTurquoise",
+      "MediumVioletRed",
+      "MidnightBlue",
+      "MintCream",
+      "MistyRose",
+      "Moccasin",
+      "NavajoWhite",
+      "Navy",
+      "OldLace",
+      "Olive",
+      "OliveDrab",
+      "Orange",
+      "OrangeRed",
+      "Orchid",
+      "PaleGoldenRod",
+      "PaleGreen",
+      "PaleTurquoise",
+      "PaleVioletRed",
+      "PapayaWhip",
+      "PeachPuff",
+      "Peru",
+      "Pink",
+      "Plum",
+      "PowderBlue",
+      "Purple",
+      "RebeccaPurple",
+      "Red",
+      "RosyBrown",
+      "RoyalBlue",
+      "SaddleBrown",
+      "Salmon",
+      "SandyBrown",
+      "SeaGreen",
+      "SeaShell",
+      "Sienna",
+      "Silver",
+      "SkyBlue",
+      "SlateBlue",
+      "SlateGray",
+      "SlateGrey",
+      "Snow",
+      "SpringGreen",
+      "SteelBlue",
+      "Tan",
+      "Teal",
+      "Thistle",
+      "Tomato",
+      "Turquoise",
+      "Violet",
+      "Wheat",
+      "White",
+      "WhiteSmoke",
+      "Yellow",
+      "YellowGreen",
     ];
     const randomIndex = Math.floor(Math.random() * simpleColorArr.length);
 
@@ -32,14 +166,27 @@ const BgFlipper = () => {
     bgContainer[0].setAttribute("style", `background-color:${randomColor} `);
     const colorTitle = randomColor[0].toUpperCase() + randomColor.slice(1);
 
-    console.log((colorTitleContainer[0].innerHTML = `Color: ${colorTitle}`));
+    colorTitleContainer[0].innerHTML = `Color: ${colorTitle}`;
+  };
+  const handleHexBgFlip = (e) => {
+    e.preventDefault();
+    var hexColor = getColor();
+    const bgContainer = document.getElementsByClassName("bg-container");
+    bgContainer[0].setAttribute("style", `background-color:${hexColor} `);
+    const colorTitleContainer = document.getElementsByClassName("bg-title");
+    colorTitleContainer[0].innerHTML = `Color: ${hexColor}`;
   };
   return (
     <div className="bg-container">
       <h1 className="bg-title">Color: Red</h1>
-      <button className="bg-btn" onClick={handlebgflip}>
-        Click for a New simple Color
-      </button>
+      <div className="btn-container">
+        <button className="bg-btn" onClick={handleBgFlip}>
+          Click for a New simple Color
+        </button>
+        <button className="bg-Hex_btn" onClick={handleHexBgFlip}>
+          Click for a New Hex Color
+        </button>
+      </div>
     </div>
   );
 };
