61248caae23b287945005797f393b13ee82ebcf3
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-28 08:41:58 -0400

    cleaned up and organized files and folders. added Header to display React Showcase

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <nav className="header">
+      <h1 className="header-title">
+        Hello, Welcome to the George Saad's React Showcase!
+      </h1>
+    </nav>
+  );
+};
+
+export default Header;
