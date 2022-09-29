e80da3c845c6847138c04d7c9d7c1583ee3a9610
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-28 22:13:56 -0400

    removed document manipulation and added state instd

--- src/components/BG_simple-Flipper/BG_Flipper.js
+++ src/components/BG_simple-Flipper/BG_Flipper.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BG_Flipper.css";
 
 const BgFlipper = () => {
+  const [backgroundColor, setBackgrounColor] = useState("red");
   const getColor = () => {
     return "#" + Math.random().toString(16).slice(2, 8);
   };
+
   const handleBgFlip = (e) => {
     e.preventDefault();
     const simpleColorArr = [
@@ -160,25 +162,19 @@ const BgFlipper = () => {
     ];
     const randomIndex = Math.floor(Math.random() * simpleColorArr.length);
 
-    const randomColor = simpleColorArr[randomIndex];
-    const bgContainer = document.getElementsByClassName("bg-container");
-    const colorTitleContainer = document.getElementsByClassName("bg-title");
-    bgContainer[0].setAttribute("style", `background-color:${randomColor} `);
-    const colorTitle = randomColor[0].toUpperCase() + randomColor.slice(1);
-
-    colorTitleContainer[0].innerHTML = `Color: ${colorTitle}`;
+    setBackgrounColor(simpleColorArr[randomIndex]);
   };
   const handleHexBgFlip = (e) => {
     e.preventDefault();
     var hexColor = getColor();
-    const bgContainer = document.getElementsByClassName("bg-container");
-    bgContainer[0].setAttribute("style", `background-color:${hexColor} `);
-    const colorTitleContainer = document.getElementsByClassName("bg-title");
-    colorTitleContainer[0].innerHTML = `Color: ${hexColor}`;
+    setBackgrounColor(hexColor);
+  };
+  const backgroundStyle = {
+    backgroundColor: backgroundColor,
   };
   return (
-    <div className="bg-container">
-      <h1 className="bg-title">Color: Red</h1>
+    <div className="bg-container" style={backgroundStyle}>
+      <h1 className="bg-title">Color: {backgroundColor}</h1>
       <div className="btn-container">
         <button className="bg-btn" onClick={handleBgFlip}>
           Click for a New simple Color
