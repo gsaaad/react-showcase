d17eef0afb3e12cdc0f5f671cc9654dd706d5b7e
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-29 17:09:52 -0400

    stylized and added tailwind carousel to animate reviews

--- src/components/ReviewCarousel/ReviewCarousel.js
+++ src/components/ReviewCarousel/ReviewCarousel.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 const ReviewCarousel = () => {
   const [componentStyle, setComponentStyle] = useState({
     display: "none",
@@ -14,6 +12,35 @@ const ReviewCarousel = () => {
       setComponentStyle({ display: "none" });
     }
   };
+  const reviews = [
+    {
+      name: "Michael J.",
+      title: "Front-end Developer",
+      avatar:
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/54.jpg",
+      stars: "⭐⭐⭐⭐",
+      description:
+        "I'm a big fan of this STOCKSCREENER. It's a simple and easy-to-use tool that I use all the time, it makes me thousands of dollars!",
+    },
+    {
+      name: "John M.",
+      title: "Back-end Developer",
+      avatar:
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/7.jpg",
+      stars: "⭐⭐⭐",
+      description:
+        "I own many shirts, but none like this one. I put this shirt on, and it immediately became my one true love. It's very special and I will never be without it.",
+    },
+    {
+      name: "Michelle J.",
+      title: "UXI Designer",
+      avatar:
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/47.jpg",
+      stars: "⭐⭐⭐⭐⭐",
+      description:
+        "Coming back to this place made it feel like home sweet home. Very nice breakfast place, food was good, presentation was creative and service was great!",
+    },
+  ];
   return (
     <div className="bg-amber-800 rounded-xl">
       <button className="bg-btn" onClick={handleShowComponent}>
@@ -22,34 +49,33 @@ const ReviewCarousel = () => {
       <div></div>
       <div
         style={componentStyle}
-        className="grid grid-cols-1 bg-amber-400 rounded-xl"
+        className="grid grid-rows-1 bg-amber-400 rounded-xl"
       >
-        <div className="grid grid-cols-3  bg-amber-500 p-4 m-4 rounded-xl">
-          <div className="h-96 bg-amber-100 rounded-xl mr-2 ">
-            <img
-              className="rounded-xl mx-auto my-8"
-              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/54.jpg"
-              alt="Avatar"
-            />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
+        <div className="flex flex-row ">
+          <button className="basis-1/4 bg-red-100 rounded-xl p-4 m-2 md:text-4xl font-semibold">
+            Back
+          </button>
+          <div className="grid grid-cols-3  bg-amber-500 p-4 m-4 rounded-xl">
+            {reviews.map((review) => {
+              return (
+                <div key={review.name} className=" lg:text-2xl">
+                  <div className="h-96 bg-amber-100 rounded-xl mr-2 ">
+                    <img
+                      className="rounded-xl mx-auto py-2"
+                      src={review.avatar}
+                      alt="Avatar"
+                    />
+                    <p>{review.stars}</p>
+                    <p>{review.description}</p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-          <div className="h-96 bg-amber-100 rounded-xl  ">
-            <img
-              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/7.jpg"
-              alt="Avatar"
-              className="rounded-xl mx-auto my-8"
-            />
-          </div>
-          <div className="h-96 bg-amber-100 rounded-xl ml-2 ">
-            <img
-              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/47.jpg"
-              alt="Avatar"
-              className="rounded-xl mx-auto my-8"
-            />
-          </div>
+
+          <button className="basis-1/4  bg-red-100 rounded-xl p-4 m-2 md:text-4xl font-semibold">
+            Next
+          </button>
         </div>
       </div>
     </div>
