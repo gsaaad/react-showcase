534cff6aa86db8232ff914ac9ee7da68faad2154
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-30 14:51:16 -0400

    added style and more feature for tic tac toe game, requires 2 players and player 1 chooses symbol

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -4,7 +4,18 @@ const TicTacToe = () => {
   const [componentStyle, setComponentStyle] = useState({
     display: "none",
   });
-  const displayArray = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+
+  const displayArray = [
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+    { symbol: "" },
+  ];
   const handleShowComponent = (e) => {
     e.preventDefault();
 
@@ -17,7 +28,8 @@ const TicTacToe = () => {
 
   const handleSymbol = (e) => {
     e.preventDefault();
-    console.log(e.target);
+
+    e.target.innerHTML = "x";
   };
   return (
     <div className="bg-amber-800 rounded-xl">
@@ -25,19 +37,36 @@ const TicTacToe = () => {
         Display TicTacToe
       </button>
       <div style={componentStyle}>
-        <div>
-          <h1 className="text-4xl bg-amber-300 font-semibold2">
-            Welcome to Tic Tac Toe~!
-          </h1>
+        <div className="grid grid-cols-2 rounded-xl">
+          <div>
+            <h1 className="text-4xl bg-amber-300 font-semibold2  rounded-xl">
+              Welcome to Tic Tac Toe~!
+            </h1>
+            <p className="text-2xl bg-amber-400 h-1/4 rounded-xl">
+              Player 1: Choose your symbol!
+            </p>
+            <button className="text-4xl bg-orange-500 w-1/2 rounded-xl">
+              X
+            </button>
+            <br />
+            <button className="text-4xl bg-orange-400 w-1/2 rounded-xl">
+              O
+            </button>
+            <p className="text-2xl bg-amber-400 h-1/4 rounded-xl">
+              Player 2: Your symbol is:{" "}
+            </p>
+          </div>
           <div className="grid w-1/2">
             <div className="grid grid-cols-3 ml-auto">
-              {displayArray.map((symbol) => {
+              {Object.values(displayArray).map((key, value) => {
                 return (
                   <button
                     className=" bg-amber-700 w-32 h-24 p-8 text-amber-200 text-4xl border-2 border-yellow-300"
                     onClick={handleSymbol}
+                    key={value}
+                    id={value}
                   >
-                    {symbol}
+                    {key.symbol}
                   </button>
                 );
               })}
