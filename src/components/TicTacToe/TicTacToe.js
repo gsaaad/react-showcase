bc138f4b583ea5fa7461c3e61b5ee32df6871cb6
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-30 11:46:11 -0400

    added simple starter component for tictactoe game, 9 buttons are displayed for players to choose an input

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -4,6 +4,7 @@ const TicTacToe = () => {
   const [componentStyle, setComponentStyle] = useState({
     display: "none",
   });
+  const displayArray = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
   const handleShowComponent = (e) => {
     e.preventDefault();
 
@@ -13,12 +14,37 @@ const TicTacToe = () => {
       setComponentStyle({ display: "none" });
     }
   };
+
+  const handleSymbol = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
     <div className="bg-amber-800 rounded-xl">
       <button className="bg-btn" onClick={handleShowComponent}>
         Display TicTacToe
       </button>
-      <div style={componentStyle}>Container</div>
+      <div style={componentStyle}>
+        <div>
+          <h1 className="text-4xl bg-amber-300 font-semibold2">
+            Welcome to Tic Tac Toe~!
+          </h1>
+          <div className="grid w-1/2">
+            <div className="grid grid-cols-3 ml-auto">
+              {displayArray.map((symbol) => {
+                return (
+                  <button
+                    className=" bg-amber-700 w-32 h-24 p-8 text-amber-200 text-4xl border-2 border-yellow-300"
+                    onClick={handleSymbol}
+                  >
+                    {symbol}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
